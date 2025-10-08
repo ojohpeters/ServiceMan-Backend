15494d7bbe6ab80998e255e12f89bdc65e60c3d3
ojochegbe <peterspraisezy>
2025-10-08 06:13:55 +0000

    Undid the comit of Fix email configuration - add DEFAULT_FROM_EMAIL

--- config/settings/production.py
+++ config/settings/production.py
@@ -27,7 +27,6 @@ EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True').lower() == 'true'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER', 'no-reply@yourdomain.com')
 
 # Redis/Celery
 REDIS_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379/1')
